228e370bb07b07cf25b3b1b2ed285180ca27a9b6
Khoi <leminhkhoi16062k5@gmail.com>
2024-04-07 14:42:30 +0700

    setState

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,11 +8,21 @@ class MyComponent extends React.Component {
   address: 'hcm',
   age: 18,
   };
+
+  handleClick(event) {
+    console.log("Click button");
+    console.log("My name is", this.state.name);
+
+    this.setState({
+      name: 'Eric'
+    })
+  }
   // JSX
   render() {
     return   (
     <div>
       My name is {this.state.name}
+      <button onClick={(event) => {this.handleClick(event)}}>Click me</button>
         </div>
         )  ;
   }
