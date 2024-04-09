54361dd0758b7b3e190780d1efe7974644216a2a
Khoi <leminhkhoi16062k5@gmail.com>
2024-04-09 14:42:21 +0700

    Conditional Output

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,6 +1,16 @@
 import React from "react";
 
 class DisplayInfo extends React.Component {
+
+    state = {
+        isShowListUser: true
+    }
+    handleShowHide = () => {
+        this.setState({
+            // !: bien true thanh false, false thanh true
+            isShowListUser: !this.state.isShowListUser
+        })
+    }
     render() {
         // destructuring array/object
         const { listUsers } = this.props;
@@ -8,16 +18,27 @@ class DisplayInfo extends React.Component {
         // props => viet tat property
         return (
             <div>
-                {listUsers.map((user) => {
-                    console.log(user)
-                    return (
-                        <div key={user.id}>
-                            <div>My name's {user.name} </div>
-                            <div>My name's {user.age} </div>
-                            <hr />
-                        </div>
-                    )
-                })}
+                <div>
+                    <span onClick={() => {
+                        this.handleShowHide()
+                    }}>
+                        {this.state.isShowListUser == true ? "Hide list user" : "Show list user"}
+                    </span>
+                </div>
+                {this.state.isShowListUser == true &&
+                    <div>
+                        {listUsers.map((user) => {
+                            console.log(user)
+                            return (
+                                <div key={user.id} className={user.age > 18 ? "green" : "red"}>
+                                    <div>My name's {user.name} </div>
+                                    <div>My name's {user.age} </div>
+                                    <hr />
+                                </div>
+                            )
+                        })}
+                    </div>
+                }
             </div>
         )
 
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,25 +4,25 @@ import React from "react";
 import UserInfo from "./UserInfo";
 import DisplayInfo from "./DisplayInfo";
 class MyComponent extends React.Component {
- state ={
-  listUsers: [
-    { id:1, name: 'Khoi', age:30},
-    { id:2, name: 'James', age:18},
-    { id:3, name: 'Tu', age:27},
+  state = {
+    listUsers: [
+      { id: 1, name: 'Khoi', age: 15 },
+      { id: 2, name: 'James', age: 18 },
+      { id: 3, name: 'Tu', age: 27 },
 
-  ]
- }
+    ]
+  }
 
   // JSX
   render() {
-    return(
-    <div>
-       <UserInfo/>
-       <br />
-       <DisplayInfo listUsers={this.state.listUsers}  />
-        </div>
-        )  ;
+    return (
+      <div>
+        <UserInfo />
+        <br />
+        <DisplayInfo listUsers={this.state.listUsers} />
+      </div>
+    );
   }
 }
 
-export default  MyComponent;
+export default MyComponent;
