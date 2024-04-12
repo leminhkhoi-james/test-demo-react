8e9a60844a92fcaf046ab3f5159b7dc8c703989f
Khoi <leminhkhoi16062k5@gmail.com>
2024-04-12 15:05:16 +0700

    stateful/stateless

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,48 +1,35 @@
 import React from "react";
 import './DisplayInfo.scss';
-class DisplayInfo extends React.Component {
+// class DisplayInfo extends React.Component {
 
-    state = {
-        isShowListUser: true
-    }
-    handleShowHide = () => {
-        this.setState({
-            // !: bien true thanh false, false thanh true
-            isShowListUser: !this.state.isShowListUser
-        })
-    }
-    render() {
-        // destructuring array/object
-        const { listUsers } = this.props;
-        // const listUsers = this.props.listUsers
-        // props => viet tat property
-        return (
-            <div>
+
+//     }
+// }
+
+const DisplayInfo = (props) => {
+    // destructuring array/object
+    const { listUsers } = props;
+    // const listUsers = this.props.listUsers
+    // props => viet tat property
+    return (
+        <div>
+            {true &&
                 <div>
-                    <span onClick={() => {
-                        this.handleShowHide()
-                    }}>
-                        {this.state.isShowListUser == true ? "Hide list user" : "Show list user"}
-                    </span>
+                    {listUsers.map((user) => {
+                        console.log(user)
+                        return (
+                            <div key={user.id} className={user.age > 18 ? "green" : "red"}>
+                                <div>My name's {user.name} </div>
+                                <div>My name's {user.age} </div>
+                                <hr />
+                            </div>
+                        )
+                    })}
                 </div>
-                {this.state.isShowListUser == true &&
-                    <div>
-                        {listUsers.map((user) => {
-                            console.log(user)
-                            return (
-                                <div key={user.id} className={user.age > 18 ? "green" : "red"}>
-                                    <div>My name's {user.name} </div>
-                                    <div>My name's {user.age} </div>
-                                    <hr />
-                                </div>
-                            )
-                        })}
-                    </div>
-                }
-            </div>
-        )
+            }
+        </div>
+    )
 
 
-    }
 }
 export default DisplayInfo
